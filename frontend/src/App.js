d18bd76125beb8eda7a572afd100029d6a569a4f
ge-nome <ebubeokerezi@gmail.com>
2022-12-08 07:04:38 +0100

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckCircle } from '@fortawesome/free-regular-svg-icons';
+import { useState } from 'react';
+
+function App() {
+  const [status, setstatus] = useState(0)
+  setTimeout(() => {
+    setstatus(1)
+  }, 3000);
+  return (
+    <div className="App">
+      <div className='box'>
+          <div className='first'>
+                <h1>PhoenixMFA</h1>
+                {
+                  status === 0 ? <h1>2:00m</h1> : ''
+                }
+                
+              </div>
+          {
+            status === 0 ?
+            <div className='body'>
+              
+              <p>Send this code</p>
+              <div className='pin'>
+                877 845
+              </div>
+              <p>to</p>
+              <div className='number'>66577</div>
+          </div>
+          :
+          <div className='body'>
+              <div className='fawes'>
+                <FontAwesomeIcon icon={faCheckCircle} />
+              </div>
+              <p>Successfully Verified</p>
+          </div>
+          }
+      </div>
+    </div>
+  );
+}
+
+export default App;
